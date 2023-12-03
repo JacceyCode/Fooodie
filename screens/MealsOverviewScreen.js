cd182d6680b5fc73e727f1af4bf8d1acb9d48694
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2023-12-03 15:47:29 +0100

    navigation & routing of screen

---
+++ screens/MealsOverviewScreen.js
@@ -0,0 +1,37 @@
+import { StyleSheet, FlatList, View } from "react-native";
+
+import { MEALS } from "../data/dummy-data";
+import MealItem from "../components/MealItem";
+
+function MealsOverviewScreen({ route }) {
+  const catId = route.params.categoryId;
+
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  const renderMealItem = (itemData) => {
+    const { item } = itemData;
+
+    return <MealItem item={item} />;
+  };
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={displayedMeals}
+        renderItem={renderMealItem}
+        keyExtractor={(item) => item.id}
+      />
+    </View>
+  );
+}
+
+export default MealsOverviewScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16,
+  },
+});
