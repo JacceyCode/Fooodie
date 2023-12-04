bba8ca6da243d42c44670f83208fd58b83cd88ed
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2023-12-04 13:33:14 +0100

    food detail completed

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,9 +1,62 @@
-import { Text } from "react-native";
+import { Image, ScrollView, StyleSheet, Text, View } from "react-native";
+
+import { MEALS } from "../data/dummy-data";
+import MealDetails from "../components/MealDetails";
+import Subtitle from "../components/MealDetail/Subtitle";
+import List from "../components/MealDetail/List";
 
 function MealDetailScreen({ route }) {
   const mealId = route.params.mealId;
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
-  return <Text>This is the Meal detail screen ({mealId})</Text>;
+  return (
+    <ScrollView style={styles.container}>
+      <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
+      <Text style={styles.title}>{selectedMeal.title}</Text>
+      <MealDetails
+        duration={selectedMeal.duration}
+        complexity={selectedMeal.complexity}
+        affordability={selectedMeal.affordability}
+        textStyle={styles.detailText}
+      />
+
+      <View style={styles.listOutContainer}>
+        <View style={styles.listContainer}>
+          <Subtitle>Ingredients</Subtitle>
+          <List data={selectedMeal.ingredients} />
+
+          <Subtitle>Steps</Subtitle>
+          <List data={selectedMeal.steps} />
+        </View>
+      </View>
+    </ScrollView>
+  );
 }
 
 export default MealDetailScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    marginBottom: 32,
+  },
+  image: {
+    width: "100%",
+    height: 250,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 24,
+    margin: 8,
+    textAlign: "center",
+    color: "white",
+  },
+  detailText: {
+    color: "white",
+  },
+  listOutContainer: {
+    alignItems: "center",
+  },
+  listContainer: {
+    width: "80%",
+  },
+});
